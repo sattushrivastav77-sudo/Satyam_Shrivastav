da43b7bc8e1930fac195860c70392ae5de907812
BEN 10 <sattushrivastav77@gmail.com>
2026-03-10 14:26:05 +0530

    first comment

---
+++ index.js
@@ -0,0 +1,20 @@
+// let a = 7;
+// function(){   
+            
+// // }
+// WHO,WHEN  WHAT (CHANGES)
+
+//keeping a trend on how your files are changing with  reference to time
+
+
+
+// STEPS TO TRACK A File
+// 1.GIT init
+// 2.GIT add.    -FOR MULTIPLE File
+//  GIT add filepath.  -single file
+// GIT commit - m 'msg'
+
+git config --global user name'Your name'
+
+git config --user.email
+                   'Your email'
